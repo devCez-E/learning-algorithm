eafd94c40730f0cf9007d9cf2c1fba16736c85a3
sungeonlee <devcez82@gmail.com>
2023-03-30 16:02:46 +0900

    04_SLL (2)
    - Get / Set
    - Insert / Remove
    - Reverse
    - Use for Stack and Queue

--- 4_SinglyLinkedLists/SL_List.js
+++ 4_SinglyLinkedLists/SL_List.js
@@ -82,18 +82,106 @@ class SinglyLinkedList{
 
         return this;
     }
+
+    get(val){
+        if(this.length < 0 || val >= this.length) return null;
+
+        var node = this.head;
+        var counter = 0;
+
+        while(counter != val){
+            node = node.next;
+            counter++;
+        }
+
+        return node;
+    }
+
+    set(idx, val){
+        var node = this.get(idx);
+        if(!node) return false;
+        else{
+            node.val = val;
+            return true;
+        }
+    }
+
+    insert(idx, val){
+        if(idx <0 || idx > this.length) return false;
+        if(idx == this.length) return !!this.push(val);
+        if(idx == 0) return !!this.unshift(val);
+        
+        var node = new Node(val);
+        var pre = this.get(idx - 1);
+        var aft = pre.next;
+
+        pre.next = node;
+        node.next = aft;
+        this.length++;
+        return true;
+        
+    }
+
+    remove(idx){
+        if(idx <0 || idx >= this.length) return false;
+        if(idx == this.length - 1) return this.pop();
+        if(idx == 0) return this.shift();
+
+        var pre = this.get(idx - 1);
+        var node = pre.next;
+        pre.next = node.next;
+        this.length--;
+
+        return node;
+    }
+
+    reverse(){
+        var node = this.head;
+        var next = null;
+        var prev;
+
+        this.head = this.tail;
+        this.tail = node;
+
+        for(let i = 0; i < this.length; i++){
+            prev = node.next;
+            node.next = next;
+            next = node;
+            node = prev;
+        }
+
+        return this;
+    }
 }
 
 var list = new SinglyLinkedList();
 list.push("HELLO");
 list.push("GOODBYE");
-list.push("99")
-
+list.push("99");
 /*console.log(list);
 console.log(list.head);
 console.log(list.head.next);
 console.log(list.head.next.next);*/
 
+console.log(list.insert(2, "999"));
 console.log(list);
-list.unshift("WoW");
+
+console.log(list.insert(5, "999"));
 console.log(list);
+
+console.log(list.insert(list.length, "777"));
+console.log(list);
+
+console.log(list.insert(0, "000"));
+console.log(list);
+
+console.log(list.remove(3));
+console.log(list);
+
+console.log(list.remove(list.length));
+console.log(list);
+
+console.log(list.remove(-1));
+console.log(list);
+
+console.log(list.reverse());
